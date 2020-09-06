b2b8125ad430afd647d12b456fc4b382403609f4
caokai2 <caokai2@lenovo.com>
2020-09-06 13:39:46 +0800

    hdfsApi

--- src/test/java/com/ustcck/bigdata/hadoop/hdfs/HDFSApp.java
+++ src/test/java/com/ustcck/bigdata/hadoop/hdfs/HDFSApp.java
@@ -1,15 +1,16 @@
 package com.ustcck.bigdata.hadoop.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.util.Progressable;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 
@@ -79,6 +80,93 @@ public class HDFSApp {
         Path src = new Path("D:\\lenovo.template");
         Path dst = new Path("/hdfsapi/test/");
         fileSystem.copyFromLocalFile(src, dst);
+    }
+
+    /**
+     * copy本地大文件到hdfs文件系统：带进度
+     */
+    @Test
+    public void copyFromLocalBigFile() throws IOException {
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(new File("D:\\soft\\jdk-8u221-windows-x64.exe")));
+
+        FSDataOutputStream out = fileSystem.create(new Path("/hdfsapi/test/jdk-8u221-windows-x64.exe"), new Progressable() {
+            @Override
+            public void progress() {
+                System.out.print(".");
+            }
+        });
+
+        IOUtils.copyBytes(in, out, 4096);
+    }
+
+    /**
+     * copy hdfs文件系统文件到本地
+     */
+    @Test
+    public void copyToLocalFile() throws IOException {
+        Path src = new Path("/hdfsapi/test/a.txt");
+        Path dst = new Path("/ustcck");
+        fileSystem.copyToLocalFile(false, src, dst, true);
+    }
+
+    /**
+     * 查看hdfs文件系统上目标文件夹下的所有文件
+     */
+    @Test
+    public void listFiles() throws IOException {
+        Path src = new Path("/hdfsapi/test/");
+        FileStatus[] fileStatuses = fileSystem.listStatus(src);
+        for (FileStatus fileStatus : fileStatuses) {
+            String path = fileStatus.getPath().toString();
+            String isDir = fileStatus.isDirectory() ? "文件夹" : "文件";
+            String permission = fileStatus.getPermission().toString();
+            long len = fileStatus.getLen();
+            short replication = fileStatus.getReplication();
+            System.out.println(path + "\t" + isDir + "\t" + permission + "\t" + len + "\t" + replication);
+        }
+    }
+
+    /**
+     * 查看hdfs文件系统上目标文件夹下的所有文件 递归列出文件夹下的所有文件
+     */
+    @Test
+    public void listFilesRecursive() throws IOException {
+        Path src = new Path("/hdfsapi/test/");
+        RemoteIterator<LocatedFileStatus> iterator = fileSystem.listFiles(src, true);
+        while (iterator.hasNext()) {
+            LocatedFileStatus fileStatus = iterator.next();
+            String path = fileStatus.getPath().toString();
+            String isDir = fileStatus.isDirectory() ? "文件夹" : "文件";
+            String permission = fileStatus.getPermission().toString();
+            long len = fileStatus.getLen();
+            short replication = fileStatus.getReplication();
+            System.out.println(path + "\t" + isDir + "\t" + permission + "\t" + len + "\t" + replication);
+        }
+    }
+
+    /**
+     * 查看文件块信息
+     */
+    @Test
+    public void getFileBlockLocations() throws IOException {
+        Path src = new Path("/hdfsapi/test/jdk-8u221-windows-x64.exe");
+        FileStatus fileStatus = fileSystem.getFileStatus(src);
+        BlockLocation[] blocks = fileSystem.getFileBlockLocations(fileStatus, 0, fileStatus.getLen());
+        for (BlockLocation block : blocks) {
+            for (String name : block.getNames()) {
+                System.out.println(name + "===" + block.getOffset() + "===" + block.getLength());
+            }
+        }
+    }
+
+    /**
+     * 删除文件
+     */
+    @Test
+    public void delete() throws IOException {
+        Path src = new Path("/hdfsapi/test/jdk-8u221-windows-x64.exe");
+        boolean b = fileSystem.delete(src, true);
+        System.out.println(b);
     }
 
     @Test
